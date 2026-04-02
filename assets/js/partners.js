37eeced4955992d1c9ebb3d335124ee6bb05c9d3
Cenk Beri <lefkadanismanlik@gmail.com>
2026-04-02 14:34:35 +0300

    partner firm added

--- assets/js/partners.js
+++ assets/js/partners.js
@@ -6,7 +6,8 @@ window.partnerLogoItems = [
   { name: "Partner 04", url: "./assets/images/partners/ozturk.jpg" },
   { name: "Partner 05", url: "./assets/images/partners/majestik.jpg" },
   { name: "Partner 06", url: "./assets/images/partners/fulgent.jpg" },
-  { name: "Partner 07", url: "./assets/images/partners/bein.jpg" }
+  { name: "Partner 07", url: "./assets/images/partners/bein.jpg" },
+  { name: "Partner 08", url: "./assets/images/partners/balarisi.jpg" }
 ];
 
 (function () {
